7a50839c8f15fbb0a0d2bb0de77d4aca51bcbded
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-07 21:52:43 +0545

    Welcome Page and files modifed

--- www/app/app.js
+++ www/app/app.js
@@ -1,9 +1,7 @@
-// Ionic Starter App
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('pasapi', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,4 +21,21 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+      .state('welcome', {
+          url: '/welcome',
+          templateUrl: 'app/welcomePage/welcome.html'
+      });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/welcome');
+
 });
